e95aed680459e72bf489a5979031479649b7c071
dananacom <163497403+dananacom@users.noreply.github.com>
2025-04-24 18:42:01 +0900

    dana2

--- TW01/Assets/2276185_YDN/Dana_OScene_Loader.cs
+++ TW01/Assets/2276185_YDN/Dana_OScene_Loader.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Dana_Scene_Loader2 : MonoBehaviour
+public class Dana_OScene_Loader : MonoBehaviour
 {
-
     // 씬 이름을 인스펙터에서 설정할 수 있도록 공개(public)
-    public string sceneToLoad = "Dana_scene";
+    public string sceneToLoad = "Original_Scene";
 
     // UI 버튼에 연결될 함수
     public void LoadOriginalScene()
     {
         SceneManager.LoadScene(sceneToLoad);
     }
-
-
 }
