58f0f72d4b55b9c4afaf30225afd36cf34038802
Ellie023 <sylin0820@ewhain.net>
2025-04-24 15:30:59 +0900

    수정

--- TW01/Assets/2171023_SYL/yelin_Pick_Controller.cs
+++ TW01/Assets/2171023_SYL/yelin_Pick_Controller.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class yelin_Pick_Controller : MonoBehaviour
 {
+
+    bool isInTheArea = false;
     int clickCounter = 0;   
     public GameObject UI;   
 
     public void Add_Click(GameObject Clone)
     {
-        clickCounter++;
-        print($"{clickCounter} 개의 클론을 클릭했습니다.");
-        Destroy(Clone);
+        if (isInTheArea)
+        {
+            clickCounter++;
+            print($"{clickCounter} 개의 클론을 클릭했습니다.");
+            Destroy(Clone);
 
-        UI.GetComponent<yelin_UI_Controller>().Display_PickCounts(clickCounter);
+            UI.GetComponent<yelin_UI_Controller>().Display_PickCounts(clickCounter);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "FPSController")
+        {
+            isInTheArea = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "FPSController")
+        {
+            isInTheArea = false;
+        }
     }
 }
